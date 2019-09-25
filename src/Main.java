7140e1ed33d5c3b0ae3960f3f964be69f12090b0
student <samoilovid951@gmail.com>
2019-09-25 11:04:00 +0300

    closes #1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int roomPerimeter = 30;
-        int roomHeight = 3;
-        int wallpaperLength = 10;
-        int wallpaperWidth = 1;
-        int cloth = roomPerimeter/wallpaperWidth;
-        int oneRoll = wallpaperLength/roomHeight;
-        int rollsNeed = cloth/oneRoll;
-        System.out.println(rollsNeed);
+        double roomPerimeter = 29;
+        double roomHeight = 3;
+        double wallpaperLength = 10;
+        double wallpaperWidth = 1;
+        double cloth = roomPerimeter/wallpaperWidth;
+        double oneRoll = wallpaperLength/roomHeight;
+        double rollsNeed = cloth/oneRoll;
+        int rollsNeedRounded = (int) Math.round(rollsNeed);
+        System.out.println(cloth);
+        System.out.println(oneRoll);
+        System.out.println(rollsNeedRounded);
     }
 }
